9479333748a9ae8ceab3b78afecacca14330ad3d
Facundo.Cretton <facundo.cretton@gmail.com>
2024-01-15 11:04:29 -0300

    Arreglos

--- src/components/Data/Categories.js
+++ src/components/Data/Categories.js
@@ -13,6 +13,9 @@ export const Categories = [
         name: 'Todos',
         // icon: <FaListUl />,
         category: 'Todos',
+        subcategories: ['Alimento', 'Juguetes', 'Cuchas'],
+
+
 
     },
 
@@ -20,7 +23,9 @@ export const Categories = [
         id: 1,
         name: 'Perros',
         // icon: <FaDog />,
-        category: 'Perros', 
+        category: 'Perros',
+        subcategories: ['AlimentoPerros', 'JuguetesPerros', 'CuchasPerros'],
+ 
 
     },
     {
@@ -28,7 +33,83 @@ export const Categories = [
         name: 'Gatos',
         // icon: <FaCat />,
         category: 'Gatos',
+        subcategories: ['AlimentoGatos', 'JuguetesGatos', 'CuchasGatos'],
+
     }
 
 
 ];
+
+
+
+
+
+// const categories = [
+    //     {
+    //         name: "Category 1",
+    //         subcategories: [
+    //             {
+    //                 name: "Subcategory 1",
+    //                 products: [
+    //                     {
+    //                         name: "Product 1",
+    //                         price: 10.99
+    //                     },
+    //                     {
+    //                         name: "Product 2",
+    //                         price: 19.99
+    //                     },
+    //                     // Add more products for Subcategory 1 as needed
+    //                 ]
+    //             },
+    //             {
+    //                 name: "Subcategory 2",
+    //                 products: [
+    //                     {
+    //                         name: "Product 3",
+    //                         price: 15.99
+    //                     },
+    //                     {
+    //                         name: "Product 4",
+    //                         price: 24.99
+    //                     },
+    //                     // Add more products for Subcategory 2 as needed
+    //                 ]
+    //             },
+    //         ]
+    //     },
+    //     {
+    //         name: "Category 2",
+    //         subcategories: [
+    //             {
+    //                 name: "Subcategory 1",
+    //                 products: [
+    //                     {
+    //                         name: "Product 5",
+    //                         price: 12.99
+    //                     },
+    //                     {
+    //                         name: "Product 6",
+    //                         price: 21.99
+    //                     },
+    //                 ]
+    //             },
+    //             {
+    //                 name: "Subcategory 2",
+    //                 products: [
+    //                     {
+    //                         name: "Product 7",
+    //                         price: 17.99
+    //                     },
+    //                     {
+    //                         name: "Product 8",
+    //                         price: 29.99
+    //                     },
+    //                 ]
+    //             },
+    //         ]
+    //     },
+    // ];
+    
+    // export default categories;
+    
---
+++ src/components/Data/ProductsData.js
@@ -0,0 +1,103 @@
+
+
+
+export const Products = [
+
+    {
+        id: 1,
+        name: 'Alimento para perros',
+        category: 'Perros',
+        subcategory: ['AlimentoPerros', 'Alimento'],
+        price: 100,
+        
+    },
+
+    {
+        id: 2,
+        name: 'Alimento para perros 2',
+        category: 'Perros',
+        subcategory: ['AlimentoPerros', 'Alimento'],
+        price: 200,
+
+    },
+
+    {
+        id: 3,
+        name: 'Alimento para gatos',
+        category: 'Gatos',
+        subcategory: ['AlimentoGatos', 'Alimento'],
+        price: 300,
+    },
+
+    {
+        id: 4,
+        name: 'Cucha para perros',
+        category: 'Perros',
+        subcategory: ['Cuchas', 'CuchasPerros'],
+        price: 400,
+    },
+
+    {
+        id: 5,
+        name: 'Cucha para gatos',
+        category: 'Gatos',
+        subcategory: ['Cuchas', 'CuchasGatos'],
+        price: 500,
+    },
+
+    {
+        id: 6,
+        name: 'Juguete para perros',
+        category: 'Perros',
+        subcategory: ['Juguetes', 'JuguetesPerros'],
+        price: 600,
+    },
+
+    {
+        id: 7,
+        name: 'Juguete para gatos',
+        category: 'Gatos',
+        subcategory: ['Juguetes', 'JuguetesGatos'],
+        price: 700,
+
+    },
+
+    {
+        id: 8,
+        name: 'Cama para perros',
+        category: 'Perros',
+        subcategory: ['Camas', 'CamasPerros'],
+        price: 800,
+    },
+
+    {
+        id: 9,
+        name: 'Cama para gatos',
+        category: 'Gatos',
+        subcategory: ['Camas', 'CamasGatos'],
+        price: 900,
+    },
+
+    {
+        id: 10,
+        name: 'Rascador para gatos',
+        category: 'Gatos',
+        subcategory: ['Juguetes', 'JuguetesGatos'],
+        price: 1000,
+    },
+
+    {
+        id: 11,
+        name: 'Rascador para gatos 2',
+        category: 'Gatos',
+        subcategory: ['Juguetes', 'JuguetesGatos' ],
+        price: 1100,
+    },
+
+
+
+
+
+
+
+]
