849778c5e468aa349d7fd4a17a0ce27312426c6f
Facundo.Cretton <facundo.cretton@gmail.com>
2024-02-01 11:18:47 -0300

    Solucion iconos

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -20,7 +20,7 @@ const reducers = combineReducers(
 const persistConfig = {
     key: 'root',
     storage,
-    // whitelist
+    whitelist: [],
 }
 
 const persistedReducer = persistReducer(persistConfig, reducers)
